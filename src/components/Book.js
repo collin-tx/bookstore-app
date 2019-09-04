8e3a924104f2cf67a287fcef52045bdd771501ab
Collin Smith <cmsmith.tx@gmail.com>
2019-09-04 12:43:37 -0500

    added random quote generator

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,10 +6,10 @@ export class Book extends Component {
         return (
                 <li className="book list-group-item">
                     <h2>{this.props.title}</h2>
-                    <img src={this.props.img} alt="book cover" />
+                    <img src={this.props.img} alt={this.props.title + "book cover"} className="book-cover" />
                     <p>{this.props.author}</p>
                     {this.props.price ? <p>{"$ " + this.props.price}</p> : <p>Not for sale</p>}
-                    <a className="btn btn-info" href={this.props.preview} target="_blank" rel="noopener noreferrer"><i className="fas fa-book-reader"></i> Preview</a>
+                    <a className="btn btn-info mr-1" href={this.props.preview} target="_blank" rel="noopener noreferrer"><i className="fas fa-book-reader"></i> Preview</a>
                     {this.props.price && <button className="btn btn-success" onClick={(e) => this.props.addToCart(e, this.props.index)}><i className="fas fa-shopping-cart"></i> Add to Cart</button>}
                 </li>
         )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Books from './Books';
+import quotes from '../quotes.json';
 
 export class Home extends Component {
     render() {
+        let quotesLength = Object.keys(quotes).length;
         return (
             <div>
+                <p>{quotes[Math.ceil(Math.random() * quotesLength)]}</p>
                 <Books />
             </div>
         )
