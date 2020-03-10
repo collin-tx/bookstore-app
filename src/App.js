046a4ebf0862062041fb4dd5d83336818dd5f7ee
collin-tx <cmsmith.tx@gmail.com>
2020-03-10 16:14:58 -0500

    adding message in App explaining the reason for this branch + how the app is evolving

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import firebase from 'firebase';
 import './App.css';
 
-
+// The original way this app was conceived and functions is essentially using firebase 
+// as not only a backend but as a global state of sorts. For obvious reasons this is not ideal
+// and needs to be changed. THUS, the app became reduxed (Reduxify - branch) for management of global state
+// which means that sign in, commenting, getting/displaying books is all managed by Redux now -- eventually
+// the goal is to make Firebase manage ONLY user specific stuff. So hopefully it will track a user's 
+// faux purchases, persist the items in their cart, etc. The first version of Reduxed bookshop seems to basically 
+//reduce the role components' local state was forced to play and allow signing in/out to be tracked globally
 
 // Adding Firebase to App
 var firebaseConfig = {
