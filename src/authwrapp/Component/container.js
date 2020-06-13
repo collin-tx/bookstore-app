3f1945c65917383ceb43a5a62a6120d5fa4dcc9d
collin-tx <cmsmith.tx@gmail.com>
2020-06-12 21:45:51 -0500

    containing the fury

---
+++ src/authwrapp/Component/container.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SignIn from '../../containers/sign-in';
+
+const AuthwrappContainer = props => {
+
+  const { isNewUser, firebase, setIsNewUser, unwrap } = props;
+
+  return (
+    <div class="authwrap-container">
+      <SignIn firebase={firebase} isNewUser={isNewUser} unwrap={unwrap} />
+      {
+        isNewUser ? (
+          <p>Already have an account? <button className="modal-sign-link" onClick={() => setIsNewUser(false)}>sign in</button></p>) : (
+          <p>Don't have an account? <button className="modal-sign-link" onClick={() => setIsNewUser(true)}>sign up</button></p>)
+      }
+    </div>
+  )
+}
+
+export default AuthwrappContainer;
