2cdd5fbf744623746dcce431d733fa274055750e
collin-tx <cmsmith.tx@gmail.com>
2020-06-20 00:34:16 -0500

    simplifying app - wrapp - bokshop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import CartContainer from './containers/Cart';
-import Nav from './components/Nav';
-import FeaturedContainer from './containers/Featured';
-import Footer from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.css';
-import firebase from 'firebase';
-import './App.css';
-import { connect } from 'react-redux';
-import { getHistory, signInUI } from './actions';
-
 import Authwrapp from './authwrapp/App';
-
+import firebase from 'firebase';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import Header from './components/Header';
+// import Home from './components/Home';
+// import CartContainer from './containers/Cart';
+// import Nav from './components/Nav';
+// import FeaturedContainer from './containers/Featured';
+// import Footer from './components/Footer';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import './App.css';
+// import { connect } from 'react-redux';
+// import { getHistory, signInUI } from './actions';
 
 var firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -29,45 +27,36 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-const database = firebase.database();
-
-const App = props => {
-  // on render, check for FB user + get purchase history
-  // Authwrapp will create or + sign them in 
-  // Unwraps to Bookshop -> welcome modal etc. -> bookshop
+const App = () => {
   
-  const { getHistory, isSignedIn } = props;
+//   return (
+//     <BrowserRouter>
+//       <Nav firebase={firebase} />
+//       <Header />
+//       <Switch>
+//         <Route path="/" render={ () => <Home firebase={firebase} />} exact />
+//         <Route path="/cart" render={ () => <CartContainer firebase={firebase} />} />
+//         <Route path="/featured" render={ ()=> <FeaturedContainer database={database} />} />
+//         <Route component={Error} />
+//       </Switch>
+//       <Footer />
+//     </BrowserRouter>
+//   );
+// }
 
-  getHistory(firebase);
+// const mapState = state => ({
+//   // signInUI: firebase => (signInUI(firebase)(dispatch)),
+//   isSignedIn: state.signedIn
+// });
 
-  if (!isSignedIn) {
-    return <Authwrapp firebase={firebase} />;
-  }
-  
-  
-  return (
-    <BrowserRouter>
-      <Nav firebase={firebase} />
-      <Header />
-      <Switch>
-        <Route path="/" render={ () => <Home firebase={firebase} />} exact />
-        <Route path="/cart" render={ () => <CartContainer firebase={firebase} />} />
-        <Route path="/featured" render={ ()=> <FeaturedContainer database={database} />} />
-        <Route component={Error} />
-      </Switch>
-      <Footer />
-    </BrowserRouter>
-  );
+// const mapDispatch = dispatch => ({
+//   // signInUI: firebase => (signInUI(firebase)(dispatch)),
+//   getHistory: firebase => (getHistory(firebase)(dispatch))
+// });
+
+// export default connect(mapState, mapDispatch)(App);
+return <Authwrapp firebase={firebase} />;
+// return 'hi'
 }
 
-const mapState = state => ({
-  // signInUI: firebase => (signInUI(firebase)(dispatch)),
-  isSignedIn: state.signedIn
-});
-
-const mapDispatch = dispatch => ({
-  // signInUI: firebase => (signInUI(firebase)(dispatch)),
-  getHistory: firebase => (getHistory(firebase)(dispatch))
-});
-
-export default connect(mapState, mapDispatch)(App);
+export default App;
