f7669605bcd119e3522a0412f54fff566cf525e2
collin-tx <cmsmith.tx@gmail.com>
2020-07-17 14:27:42 -0500

    auth looking better, still needs work

--- src/authwrapper/sign-in/SignIn.js
+++ src/authwrapper/sign-in/SignIn.js
@@ -5,19 +5,24 @@ const SignIn = ({
   handleEmail,
   handlePassword,
   handleSubmit,
-  password
+  password,
+  devCred
 }) => (
-  <form id="sign-in-form" className="form-horizontal" onSubmit={(e) => handleSubmit(e, email, password, password)}>
-    <div className="form-group">
-      <label htmlFor="email">email:</label>
-      <input type="text" onChange={handleEmail} value={email} className="form-control" />
+  <form id="sign-in-form" className="form-horizontal auth-form" onSubmit={(e) => handleSubmit(e, email, password, password)}>
+    <div class="auth-form--signin">
+      <div className="form-group">
+        <label htmlFor="email">email:</label>
+        {/* <p>{devCred[0]}</p> */}
+        <input type="text" onChange={handleEmail} value={email} className="form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">password:</label>
+        {/* <p>{devCred[1]}</p> */}
+        <input type="password" onChange={handlePassword} value={password} className="form-control" />
+      </div>
     </div>
-    <div className="form-group">
-      <label htmlFor="password">password:</label>
-      <input type="password" onChange={handlePassword} value={password} className="form-control" />
-    </div>
-    <button className="btn btn-secondary">
-        Sign In
+    <button className="btn btn-secondary form-control">
+      Sign In
     </button>
   </form>
 );
--- src/authwrapper/sign-in/SignUp.js
+++ src/authwrapper/sign-in/SignUp.js
@@ -11,26 +11,28 @@ const SignUp = ({
   passwordVerify,
   username
 }) => (
-    <form id="sign-up-form" className="form-horizontal" onSubmit={e => handleSubmit(e, email, password, passwordVerify, true)}>
-    <div className="form-group">
-        <label htmlFor="username">username:</label>
-        <input type="text" onChange={handleUsername} value={username} className="form-control" />
-    </div>
-    <div className="form-group">
-        <label htmlFor="email">email:</label>
-        <input type="text" onChange={handleEmail} value={email} className="form-control" />
-    </div>
-    <div className="form-group">
-        <label htmlFor="password">password:</label>
-        <input type="password" onChange={handlePassword} value={password} className="form-control" />
-    </div>
-    <div className="form-group">
-        <label htmlFor="passwordVerify">confirm password:</label>
-        <input type="password" onChange={handlePasswordVerify} value={passwordVerify} className="form-control" />
-    </div>
-    <button className="btn btn-secondary form-control">
-        Sign Up
-    </button>
+    <form id="sign-up-form" className="form-horizontal auth-form" onSubmit={e => handleSubmit(e, email, password, passwordVerify, true)}>
+        <div class="auth-form--signup">
+            <div className="form-group col1">
+                <label htmlFor="username">username:</label>
+                <input type="text" onChange={handleUsername} value={username} className="form-control" />
+            </div>
+            <div className="form-group col2">
+                <label htmlFor="email">email:</label>
+                <input type="text" onChange={handleEmail} value={email} className="form-control" />
+            </div>
+            <div className="form-group col1">
+                <label htmlFor="password">password:</label>
+                <input type="password" onChange={handlePassword} value={password} className="form-control" />
+            </div>
+            <div className="form-group col2">
+                <label htmlFor="passwordVerify">confirm password:</label>
+                <input type="password" onChange={handlePasswordVerify} value={passwordVerify} className="form-control" />
+            </div>
+        </div>
+        <button className="btn btn-secondary form-control">
+            Sign Up
+        </button>
     </form>
 );
 
