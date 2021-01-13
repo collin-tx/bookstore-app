72fb5c28b4b993e7cb6823ae65d1f91c2836657c
collin-tx <cmsmith.tx@gmail.com>
2021-01-13 16:51:24 -0600

    updates App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
+import { initializeApp } from 'firebase';
+
+
+jest.mock('firebase', () => ({
+  initializeApp: () => jest.fn(),
+  apps: {
+    length: 0
+  },
+  auth: () => ({
+    signOut: () => jest.fn(),
+    currentUser: null
+  }),
+}));  
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+  , div);
   ReactDOM.unmountComponentAtNode(div);
 });
