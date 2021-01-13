690447903ed75fb34edf3d30f95ce002dba9c556
collin-tx <cmsmith.tx@gmail.com>
2021-01-13 15:46:41 -0600

    configuring jest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Auth from './authwrapper/App';
-import { initialize } from './db-bookshop';
-import store from './store';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-const App = () => <Auth firebase={initialize()(store.dispatch)} />;
+const App = () => <Auth />;
 
 export default App;
