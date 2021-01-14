fba64e4ca9b55a688bc75afcb520f841c1df4ed9
collin-tx <cmsmith.tx@gmail.com>
2021-01-14 15:53:15 -0600

    improves purchase history modal

--- src/bookshop/UserHistory/modal.js
+++ src/bookshop/UserHistory/modal.js
@@ -6,20 +6,21 @@ import UserHistory from './index';
 import { getHistory } from '../../actions/selectors';
 
 const UserHistoryModal = props => {
-    // render error fx goes here
+
+  const { onHide, show, userHistory} = props;
+
   return (
-    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal show={show} onHide={onHide} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
           Purchase History
         </Modal.Title>
-        {/* renderErrors() */}
       </Modal.Header>
       <Modal.Body>
-          <UserHistory userHistory={props.userHistory} />
+          <UserHistory userHistory={userHistory} />
       </Modal.Body>
       <Modal.Footer className="d-flex justify-content-between">
-        <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
@@ -29,18 +30,16 @@ export const UserHistoryModalContainer = props => {
   
   const [ modalShow, setModalShow ] = React.useState(false);
   return (
-      <ButtonToolbar>
-            <Button variant="primary" onClick={() => setModalShow(true)} id="show-purchase-history" className="navLink mr-1">
-                Past Orders
-            </Button> 
-        <UserHistoryModal 
-            onHide={() => setModalShow(false)}
-            show={modalShow}
-            firebase={props.firebase}
-            error={props.error}
-            userHistory={props.userHistory}
-        />
-      </ButtonToolbar>
+    <ButtonToolbar>
+      <Button variant="primary" onClick={() => setModalShow(true)} id="show-purchase-history" className="navLink mr-1">
+          past orders
+      </Button> 
+      <UserHistoryModal 
+          onHide={() => setModalShow(false)}
+          show={modalShow}
+          {...props}
+      />
+    </ButtonToolbar>
   );
 }
 
