dc48960e2a83673b4b0410999b8668e6b6117d87
collin-tx <cmsmith.tx@gmail.com>
2021-01-14 15:46:26 -0600

    better structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Auth from './authwrapper/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
-const App = () => <Auth />;
+const App = () => (
+  <Provider store={store}>
+    <Auth />
+  </Provider>
+);
 
 export default App;
