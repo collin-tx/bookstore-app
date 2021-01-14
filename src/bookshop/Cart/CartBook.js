4250f48f6bb3a1eede4659ef06258b3c7a3889fa
collin-tx <cmsmith.tx@gmail.com>
2021-01-14 17:03:34 -0600

    restyles books in cart

--- src/bookshop/Cart/CartBook.js
+++ src/bookshop/Cart/CartBook.js
@@ -15,35 +15,34 @@ const CartBook = ({
 }) => (
   <div key={generateKey(book.id)}>
     <li className="list-group-item cartBook">
-      
-      <div className="col">
-        <h2>{title}</h2>
-        <p>{subtitle}</p>
-        <img src={img} className="cart-book-img" alt="book cover" />
-        <p>{author}</p>
-        <p>${price.toFixed(2)}</p>
+      <div className="d-flex flex-columns align-items-center">
+        <div className="col">
+          <h2>{title}</h2>
+          <p>{subtitle}</p>
+          <img src={img} className="cart-book-img" alt="book cover" />
+          <p>{author}</p>
+          <p>${price.toFixed(2)}</p>
+        </div>
+        <div className="col">
+          <p>
+            {
+              !description ? 'No description available' : description.length > 500 ? description.slice(0, 500) + "..." : description
+            }
+          </p>
+          <a className="btn btn-info cart-btn" href={link} rel="noopener noreferrer" target="_blank">
+            <i className="fas fa-info"></i> 
+            {" more info"}
+          </a>
+          <a className="btn btn-info cart-btn" href={preview} target="_blank" rel="noopener noreferrer">
+            <i className="fas fa-book-reader"></i> 
+            {" preview"}
+          </a>
+          <button className="btn btn-danger cart-btn" onClick={() => remove(book)}>
+            <i className="fas fa-minus-circle"></i> 
+            {" remove"}
+          </button>
+        </div>
       </div>
-
-      <div className="col">
-        <p>
-          {
-            !description ? 'No description available' : description.length > 500 ? description.slice(0, 500) + "..." : description
-          }
-        </p>
-        <a className="btn btn-info cart-btn" href={link} rel="noopener noreferrer" target="_blank">
-          <i className="fas fa-info"></i> 
-          {" more info"}
-        </a>
-        <a className="btn btn-info cart-btn" href={preview} target="_blank" rel="noopener noreferrer">
-          <i className="fas fa-book-reader"></i> 
-          {" preview"}
-        </a>
-        <button className="btn btn-danger cart-btn" onClick={() => remove(book)}>
-          <i className="fas fa-minus-circle"></i> 
-          {" remove"}
-        </button>
-      </div>
-      
     </li>
   </div>
 );
