fe2ddb5c5ea4d36b04a9f96ec29ae64ca08c953e
collin-tx <cmsmith.tx@gmail.com>
2021-02-05 16:07:52 -0600

    updates book container with favoriting ability (wip)

--- src/bookshop/Book/container.js
+++ src/bookshop/Book/container.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Book from './Book';
 
-const BookContainer = props => (
-    <Book {...props} />
-);
+const BookContainer = props => {
+    
+    // TODO: call action to read user fb -- if any of these books match the ones they've favorited, it's favorited prop = true
+    const [ favorited, setFavorited ] = useState(false) 
+    const onClickFavorite = () => {
+        // TODO: dispatch action to update boolean value on book of users fb
+        setFavorited(!favorited);
+    };
+
+    return (
+        <Book {...props} favorited={favorited} onClickFavorite={onClickFavorite} />
+    );
+}
 
 export default BookContainer;
