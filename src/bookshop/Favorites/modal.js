1a3d238e1a922b02c5fce3f81f7d026660f5530d
collin-tx <cmsmith.tx@gmail.com>
2021-04-28 16:33:53 -0500

    bye connect

--- src/bookshop/Favorites/modal.js
+++ src/bookshop/Favorites/modal.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Modal, Button, ButtonToolbar } from 'react-bootstrap';
-
 import { getFavorites } from '../../actions/selectors';
-
 import Favorites from './index';
 
-const FavoritesModal = props => {
-    // render error fx goes here
-
-  const { onHide, show } = props;
-
+const FavoritesModal = ({
+  favorites,
+  onHide,
+  show
+}) => {
   return (
     <Modal show={show} onHide={onHide} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
           Favorites
         </Modal.Title>
-        {/* renderErrors() */}
       </Modal.Header>
       <Modal.Body>
-          <Favorites Favorites={props.favorites} />
+          <Favorites Favorites={favorites} />
       </Modal.Body>
       <Modal.Footer className="d-flex justify-content-between">
-        <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
@@ -32,28 +29,21 @@ const FavoritesModal = props => {
 export const FavoritesModalContainer = props => {
   
   const [ modalShow, setModalShow ] = React.useState(false);
+  
+  const favorites = useSelector(getFavorites);
+
   return (
-      <ButtonToolbar>
-            <Button variant="primary" onClick={() => setModalShow(true)} id="show-purchase-history" className="navLink mr-1">
-                favorites
-            </Button> 
-        <FavoritesModal 
-            onHide={() => setModalShow(false)}
-            show={modalShow}
-            firebase={props.firebase}
-            error={props.error}
-            Favorites={props.favorites}
-        />
-      </ButtonToolbar>
+    <ButtonToolbar>
+      <Button variant="primary" onClick={() => setModalShow(true)} id="show-purchase-history" className="navLink mr-1">
+        favorites
+      </Button> 
+      <FavoritesModal 
+        onHide={() => setModalShow(false)}
+        show={modalShow}
+        favorites={favorites}
+      />
+    </ButtonToolbar>
   );
 }
 
-const mapState = state => {
-    return {
-        user: state.user,
-        favorites: getFavorites(state),
-        error: state.error
-    }
-}
-
-export default connect(mapState)(FavoritesModalContainer);
+export default FavoritesModalContainer;
