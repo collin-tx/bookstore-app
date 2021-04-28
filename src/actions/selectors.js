6805b369990e8f9e0219664173cd5b54a941a1af
collin-tx <cmsmith.tx@gmail.com>
2021-04-27 22:21:42 -0500

    another one (selector)

--- src/actions/selectors.js
+++ src/actions/selectors.js
@@ -5,6 +5,7 @@ export const getFavorites = (state = []) => state.favorites ? state.favorites : 
 export const getFeatured = (state = []) => state.featured ? state.featured : {};
 export const getFirebase = (state = []) => state.firebase;
 export const getHistory = (state = []) => state.userHistory ? state.userHistory : [];
+export const getNoBooksFound = (state = []) => state.noBooks ? true : false;
 export const getQueries = (state = []) => state.queries ? state.queries : [];
 export const getSuggestions = (state = []) => state.suggestions ? state.suggestions : [];
 export const getUser = (state = []) => state.user ? state.user : {};
