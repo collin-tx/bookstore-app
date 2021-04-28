99c2a6585df65372d8e3f1c190e4c5a01a9a1d1c
collin-tx <cmsmith.tx@gmail.com>
2021-04-28 16:33:29 -0500

    these modals suck!

--- src/bookshop/UserHistory/modal.js
+++ src/bookshop/UserHistory/modal.js
@@ -1,54 +1,46 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Modal, Button, ButtonToolbar } from 'react-bootstrap';
 
 import UserHistory from './index';
 import { getHistory } from '../../actions/selectors';
 
-const UserHistoryModal = props => {
+const UserHistoryModal = ({
+  onHide,
+  show,
+  userHistory
+}) => (
+  <Modal show={show} onHide={onHide} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+    <Modal.Header closeButton>
+      <Modal.Title id="contained-modal-title-vcenter">
+        Purchase History
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        <UserHistory userHistory={userHistory} />
+    </Modal.Body>
+    <Modal.Footer className="d-flex justify-content-between">
+      <Button onClick={onHide}>Close</Button>
+    </Modal.Footer>
+  </Modal>
+);
 
-  const { onHide, show, userHistory} = props;
+export const UserHistoryModalContainer = () => {
 
-  return (
-    <Modal show={show} onHide={onHide} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Purchase History
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-          <UserHistory userHistory={userHistory} />
-      </Modal.Body>
-      <Modal.Footer className="d-flex justify-content-between">
-        <Button onClick={onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
-  
-export const UserHistoryModalContainer = props => {
-  
   const [ modalShow, setModalShow ] = React.useState(false);
+
   return (
     <ButtonToolbar>
       <Button variant="primary" onClick={() => setModalShow(true)} id="show-purchase-history" className="navLink mr-1">
-          past orders
+        past orders
       </Button> 
       <UserHistoryModal 
-          onHide={() => setModalShow(false)}
-          show={modalShow}
-          {...props}
+        onHide={() => setModalShow(false)}
+        show={modalShow}
+        userHistory={useSelector(getHistory)}
       />
     </ButtonToolbar>
   );
 }
 
-const mapState = state => {
-    return {
-        user: state.user,
-        userHistory: getHistory(state),
-        error: state.error
-    }
-}
-
-export default connect(mapState)(UserHistoryModalContainer);
+export default UserHistoryModalContainer;
