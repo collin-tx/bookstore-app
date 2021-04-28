3e486c2715f1ed054586774c6c24e96f4082851d
collin-tx <cmsmith.tx@gmail.com>
2021-04-27 22:37:04 -0500

    removes old code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,12 @@
 import { 
-  EMPTY_CART,
-  FETCH_BOOKS,
   FIREBASE,
   GET_HISTORY,
-  GET_QUERY_LOG,
   LOADING,
-  NO_BOOKS,
   RENDER_ERROR,
   REMOVE_ERROR,
   SIGN_OUT,
   SYNC_CART,
 } from './constants';
-
-import {
-  getUser,
-  getQueries,
-  getFirebase
-} from './selectors';
-import store from '../store';
 
 
 export const signOut = firebase => {
@@ -25,84 +14,6 @@ export const signOut = firebase => {
   return {
     type: SIGN_OUT
   }
-}
-
-export const addBookToCart = (firebase, book) => dispatch => {
-
-  // add book to users cart in FB here
-  // books organized in cart by book.id
-
-  const user = getUser(store.getState());
-  if (!!user){
-    const userId = user && user.uid;
-    const database = firebase.database();
-    database.ref('users/' + userId +'/cart/' + book.id)
-      .set({
-        book
-    });
-    const cartRef = database.ref('users/' + userId + '/cart');
-    cartRef.on('value', async (snapshot) => {
-      const fbCart = await snapshot.val();
-      let fbCartArr = [];
-
-      // eslint-disable-next-line
-      for (let id in fbCart){
-        fbCartArr.push(fbCart[id]);
-      }
-
-      dispatch({
-        type: SYNC_CART, 
-        payload: fbCartArr
-      });
-    });
-  }
-}
-
-export const removeBookFromCart = (firebase, book) => {
-  // find the book and suck it outta there
-    const user = getUser(store.getState());
-    // const userId = getUserId(user);
-    const userId = user && user.uid;
-    const database = firebase.database();
-    
-    database.ref('users/' + userId + '/cart/' + book.id)
-    .remove();
-    
-    let fbCartArr = [];
-    const cartRef = database.ref('users/' + userId + '/cart');
-    cartRef.on('value', async (snapshot) => {
-      const fbCart = await snapshot.val();
-      // eslint-disable-next-line
-      for (let id in fbCart){
-        fbCartArr.push(fbCart[id]);
-      }
-    });
-      
-    return {
-      type: SYNC_CART,
-      payload: fbCartArr
-    }
-}
-
-export const checkOut = (firebase, order, subtotal) => dispatch => {
-  const database = firebase.database();
-  const user = getUser(store.getState());
-  const userId = user && user.uid;
-
-  // push purchase details to purchaseHistory field on User  
-  if (!!user){
-    const path = `users/${userId}/purchaseHistory`;
-
-    database.ref(path)
-      .push({
-        order,
-        subtotal,
-        date: Date()
-      });
-  }
-  // empty cart and refresh userhistory
-  dispatch(emptyCart(firebase));
-  dispatch(getHistory(firebase, userId));
 }
 
 export const getHistory = (firebase, userId = null) => dispatch => {
@@ -134,58 +45,6 @@ export const removeError = () => {
   }
 }
 
-export const emptyCart = firebase => {
-  const user = getUser(store.getState());
-  // const userId = getUserId(user);
-  const userId = user && user.uid;
-
-  const database = firebase.database();
-
-  database.ref('users/' + userId + '/cart/')
-    .set([]);
-
-  return {
-    type: EMPTY_CART
-  }
-}
-
-export const emptyCartUI = () => {
-  return {
-    type: EMPTY_CART
-  }
-}
-
-// get books
-const fetchRequest = query => fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${process.env.REACT_APP_GOOGLE_BOOKS_API_KEY}&maxResults=18`);
-
-export const fetchBooks = query => async dispatch => {
-  dispatch(removeError())
-  await fetchRequest(query)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      dispatch({
-        type: FETCH_BOOKS,
-        payload: [data],
-        query
-      });
-      if (data && !data.items){
-        dispatch({
-          type: NO_BOOKS
-        });
-      }
-    })
-    .then(() => dispatch(logQuery(query, getFirebase(store.getState()))))
-    .catch(error => {
-      dispatch({
-        type: RENDER_ERROR,
-        payload: {error}
-      });
-  });
-}
-
-
 // sync UI cart with FB cart
 export const syncCart = firebase => dispatch => {
   const user = firebase.auth().currentUser;
@@ -214,37 +73,6 @@ export const isLoading = bool => ({
   payload: bool
 });
 
-export const storeQueryLog = firebase  => dispatch => {
-  const user = firebase.auth().currentUser;
-  const queryLogRef = firebase.database().ref('users/' + (user && user.uid) + '/log');
-
-  queryLogRef.on('value', async (snapshot) => {
-    const log = await snapshot.val();
-    let logArr = [];
-    // eslint-disable-next-line 
-    for (let q in log){
-      // logArr.push(log[q]);
-      logArr.push(log[q]);
-    }
-    dispatch({
-      type: GET_QUERY_LOG,
-      payload: logArr
-    });
-  });
-}
-
-export const logQuery = (query, firebase) => dispatch => {
-  const user = firebase.auth().currentUser;
-  if (!!user) {
-    const userId = user && user.uid;
-    const database = firebase.database();
-    const count = 'Q' + String(getQueries(store.getState()).length + 1);
-    database.ref('users/' + userId +'/log/' + count)
-      .set({
-        query,
-        date: Date.now()
-    })
-      .then(() => storeQueryLog(firebase))(dispatch);
-  }
-  return false;
-}
+// export const getSuggestions = firebase => {
+    // database.ref('suggestions/')
+// }
