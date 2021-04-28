c7afda3395aa423a1f0ccaf878a82fcfc903491b
collin-tx <cmsmith.tx@gmail.com>
2021-04-28 16:28:47 -0500

    cart container is now beautful

--- src/bookshop/Cart/container.js
+++ src/bookshop/Cart/container.js
@@ -1,97 +1,98 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import CartBook from './CartBook';
 import Cart from './Cart';
-import { emptyCart, removeBookFromCart } from '../../entities/cart';
+
+import {
+    emptyCart as emptyCartAction,
+    removeBookFromCart
+} from '../../entities/cart';
+
 import { generateKey } from '../../utils/helper';
+import {
+    getCart,
+    getFirebase,
+    getHistory,
+    getUser 
+} from '../../actions/selectors';
 
-export class CartContainer extends Component {  
+const CartContainer = () => {
 
-    handleRemove = book => {
-        this.props.removeBookFromCart(this.props.firebase, book, this.props.user);
-    }
+    const dispatch = useDispatch();
 
-    emptyCart = () => {
-        this.props.emptyCart(this.props.firebase);
-    }
-
-    render() {
-        let booksInCart = !!this.props.cart && this.props.cart.map(record => {
-            return (
-                <CartBook title={record.book.volumeInfo.title} book={record.book}
-                author={record.book.volumeInfo.authors && record.book.volumeInfo.authors[0]}
-                img={record.book.volumeInfo.imageLinks && record.book.volumeInfo.imageLinks.thumbnail}
-                subtitle={record.book.volumeInfo.subtitle && record.book.volumeInfo.subtitle}
-                description={record.book.volumeInfo.description && record.book.volumeInfo.description}
-                key={record.book.id} id={record.book.etag} remove={this.handleRemove}
-                link={record.book.volumeInfo.infoLink} preview={record.book.volumeInfo.previewLink}
-                price={record.book.saleInfo.listPrice.amount}
-                />
-            );
-        });
-            
-        let priceArray = this.props.cart && this.props.cart.length && this.props.cart.map(book => book.book.saleInfo.listPrice.amount);
-        const subtotal = priceArray ? priceArray.reduce((a,b) => a + b) : 0;
-
-        let checkoutBooks = (
-            <div>
-                <table className="checkout-table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            !!this.props.cart && this.props.cart.map((book, index) => {
-                                return (
-                                    <tr key={generateKey(book.id)}>
-                                        <td>{book.book.volumeInfo.title}</td>
-                                        <td>{book.book.volumeInfo.authors && book.book.volumeInfo.authors[0]}</td>
-                                        <td>${book.book.saleInfo.listPrice.amount.toFixed(2)}</td>
-                                    </tr>
-                                );
-                            })
-                        }
-                    </tbody>
-                </table>
-                <br />
-                <div className="flex-column">
-                    <p><b>Subtotal</b></p>
-                    <p>${subtotal.toFixed(2)}</p>
-                </div>
-
-            </div>   
-        );
+    const user = useSelector(getUser);
+    const cart = useSelector(getCart);
+    const userHistory = useSelector(getHistory);
+    const firebase = useSelector(getFirebase);
     
+    const handleRemove = book => {
+        dispatch(removeBookFromCart(firebase, book, user));
+    }
+
+    const handleEmptyCart = () => {
+        dispatch(emptyCartAction(firebase, user));
+    }
+
+    const booksInCart = cart.map(record => (
+        <CartBook title={record.book.volumeInfo.title} book={record.book}
+            author={record.book.volumeInfo.authors && record.book.volumeInfo.authors[0]}
+            img={record.book.volumeInfo.imageLinks && record.book.volumeInfo.imageLinks.thumbnail}
+            subtitle={record.book.volumeInfo.subtitle && record.book.volumeInfo.subtitle}
+            description={record.book.volumeInfo.description && record.book.volumeInfo.description}
+            key={record.book.id} id={record.book.etag} remove={handleRemove}
+            link={record.book.volumeInfo.infoLink} preview={record.book.volumeInfo.previewLink}
+            price={record.book.saleInfo.listPrice.amount}
+        />
+    ));
+            
+    const priceArray = cart.length && cart.map(book => book.book.saleInfo.listPrice.amount);
+    const subtotal = priceArray ? priceArray.reduce((a,b) => a + b) : 0;
+
+    const checkoutBooks = (
+        <div>
+            <table className="checkout-table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        !!cart && cart.map((book, index) => {
+                            return (
+                                <tr key={generateKey(book.id)}>
+                                    <td>{book.book.volumeInfo.title}</td>
+                                    <td>{book.book.volumeInfo.authors && book.book.volumeInfo.authors[0]}</td>
+                                    <td>${book.book.saleInfo.listPrice.amount.toFixed(2)}</td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+            <br />
+            <div className="flex-column">
+                <p><b>Subtotal</b></p>
+                <p>${subtotal.toFixed(2)}</p>
+            </div>
+
+        </div>   
+    );
+
     return (
         <Cart 
             booksInCart={booksInCart}
-            cart={this.props.cart}
+            cart={cart}
             checkoutBooks={checkoutBooks}
-            emptyCart={this.emptyCart}
-            firebase={this.props.firebase}
+            emptyCart={handleEmptyCart}
+            firebase={firebase}
             subtotal={subtotal}
-            user={(this.props.user && this.props.user.displayName)}
-            userHistory={this.props.userHistory}
+            user={(user && user.displayName)}
+            userHistory={userHistory}
         />
-        )
-    }
+    );
 }
 
-const mapState = state => {
-    return {
-        user: state.user,
-        cart: state.cart,
-        userHistory: state.userHistory
-    }
-}
-
-const mapDispatch = dispatch => ({ 
-    removeBookFromCart: (fb, book, u) => dispatch(removeBookFromCart(fb, book, u)),
-    emptyCart: fb => dispatch(emptyCart(fb))
-});
-
-export default connect(mapState, mapDispatch)(CartContainer);
+export default CartContainer;
