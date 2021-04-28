03f4c06de3b9120741295dcbbaa308753a80a4ef
collin-tx <cmsmith.tx@gmail.com>
2021-04-28 16:27:52 -0500

    converts signin container

--- src/authwrapper/sign-in/container.js
+++ src/authwrapper/sign-in/container.js
@@ -1,154 +1,120 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import {
+  useDispatch,
+  useSelector
+} from 'react-redux';
 
 import SignIn from './SignIn'
 import SignUp from './SignUp';
 
-import { signOut } from '../../actions';
-import { signIn, unwrap, createUser} from './actions';
-import { removeError, renderError } from '../../actions';
+import { 
+  signIn, 
+  createUser as createUserAction
+} from './actions';
+
+import { renderError } from '../../actions';
 import { validateEmail, validatePassword } from '../../utils/helper';
+import {
+  getError,
+  getUser
+} from '../../actions/selectors';
 
-class SignInContainer extends Component {
+const SignInContainer = ({
+  firebase,
+  isNewUser
+}) => {
+  const dispatch = useDispatch();
 
-  state = {
-    email: '',
-    password: '',
-    passwordVerify: '',
-    username: ''
+  // dev helper-- remove below
+  // dispatch(signIn(firebase, 'a@aa.com', 'aaaaaaa7'));
+  // remove above this
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ passwordVerify, setPasswordVerify ] = useState('');
+  const [ username, setUsername ] = useState('');
+
+  const user = useSelector(getUser);
+  const error = useSelector(getError);
+  const isSignedIn = !!user?.uid;
+
+  const handleEmail = e => {
+    setEmail(e.target.value);
   }
 
-  componentDidMount(){
-    const user = this.props.firebase.auth().currentUser;
-    if (!!user){
-      if (!this.state.isSignedIn) {
-        this.props.unwrap(this.props.firebase);
-      }
-      
-    } else {
-      this.props.signOut(this.props.firebase);
-    }
+  const handlePassword = e => {
+    setPassword(e.target.value);
   }
 
-  handleEmail = e => {
-    this.setState({ email: e.target.value });
+  const handlePasswordVerify = e => {
+    setPasswordVerify(e.target.value);
   }
 
-  handlePassword = e => {
-    this.setState({ password: e.target.value });
+  const handleUsername = e => {
+    setUsername(e.target.value);
   }
 
-  handlePasswordVerify = e => {
-    this.setState({ passwordVerify: e.target.value });
+  const loginUser = () => {
+    console.log(dispatch);
+    isNewUser ? createUserAction(firebase, email, password, username)(dispatch) :
+    signIn(firebase, email, password)(dispatch)
   }
 
-  handleUsername = e => {
-    this.setState({ username: e.target.value })
-  }
+  const handleSubmit = e => {
 
-  handleSubmit = (e, email, password, passwordVerify, newUser = false) => {
     e.preventDefault();
 
-    if (validateEmail(email) && validatePassword(password, passwordVerify)) {
-      if (this.props.isNewUser) {
-        if (!!this.state.username.length){
+    // if (validateEmail(email) && validatePassword(password, passwordVerify)) {
+      // if (!!username.length){
+        loginUser();
+      // }
+    // }
 
-          this.props.createUser(this.props.firebase, this.state.email, this.state.password, this.state.username);
-          this.props.unwrap(this.props.firebase);
-        }
-      }
-      
-      if (!this.props.isNewUser) {
-        this.loginUser(this.state.email, this.state.password);
-        const user = this.props.firebase.auth().currentUser
-        if (!!user) {
-          this.props.unwrap(this.props.firebase);
-        } 
-      }
-    }
-    
     // errors
     // todo: clean THIS UP oof
     if (!validateEmail(email)) {
-      this.props.renderError({ error: { message: 'Please use a proper email address' } });
+      renderError({ error: { message: 'Please use a proper email address' } });
     } else if (!validatePassword(password, passwordVerify)) {
-      this.props.renderError({ error: { message: newUser ? 'Passwords must match and be at least 6 characters' : 'Incorrect Password' } });
-    } else if (!this.state.username.length && newUser){
-      this.props.renderError({ error: { message: 'Please enter a username' } });
+      renderError({ error: { message: isNewUser ? 'Passwords must match and be at least 6 characters' : 'Incorrect Password' } });
+    } else if (!username.length && isNewUser){
+      renderError({ error: { message: 'Please enter a username' } });
     }
 
   }
 
-  loginUser = (email, password) => {
-    const { firebase } = this.props;
-    this.props.signIn(firebase, email, password);
-  }
+  const renderSignUp = () => (
+    <SignUp 
+      email={email}
+      error={error}
+      handleEmail={handleEmail}
+      handlePassword={handlePassword}
+      handlePasswordVerify={handlePasswordVerify}
+      handleSubmit={handleSubmit}
+      handleUsername={handleUsername}
+      password={password}
+      passwordVerify={passwordVerify}
+      username={username}
+    /> 
+  );
 
-  renderSignUp = () => {
+  const renderSignIn = () => (
+    <SignIn
+      user={user}
+      isSignedIn={isSignedIn}
+      email={email}
+      error={error}
+      handleEmail={handleEmail}
+      handlePassword={handlePassword}
+      handleSubmit={handleSubmit}
+      password={password}
+    />
+  );
 
-    const { email, password, passwordVerify, username } = this.state;
-    const { error } = this.props;
-    
-    return (
-      <SignUp 
-          email={email}
-          error={error}
-          handleEmail={this.handleEmail}
-          handlePassword={this.handlePassword}
-          handlePasswordVerify={this.handlePasswordVerify}
-          handleSubmit={this.handleSubmit}
-          handleUsername={this.handleUsername}
-          password={password}
-          passwordVerify={passwordVerify}
-          username={username}
-        /> 
-    );
-  }
-
-  renderSignIn = () => {
-
-    const { email, password } = this.state;
-    const { error, user, isSignedIn } = this.props;
-
-    return (
-      <SignIn
-            user={user}
-            isSignedIn={isSignedIn}
-            email={email}
-            error={error}
-            handleEmail={this.handleEmail}
-            handlePassword={this.handlePassword}
-            handleSubmit={this.handleSubmit}
-            password={password}
-          />
-
-      );
-  }
-
-
-  render() {
-    const { isNewUser } = this.props;
-    return (
-      <div className="authwrapper">
-        {isNewUser ? this.renderSignUp() : this.renderSignIn()}
-      </div>
-    );
-  }
+  return (
+    <div className="authwrapper">
+      {isNewUser ? renderSignUp() : renderSignIn()}
+    </div>
+  );
 }
 
-const mapState = state => ({
-    user: state.user,
-    isSignedIn: state.isSignedIn,
-    error: state.error
-});
-
-const mapDispatch = dispatch => ({
-    signIn: (firebase, email, password) => dispatch(signIn(firebase, email, password)),
-    signOut: firebase => dispatch(signOut(firebase)),
-    unwrap: firebase => dispatch(unwrap(firebase)),
-    createUser: (firebase, email, password, username) => dispatch(createUser(firebase, email, password, username)),
-    removeError: () => dispatch(removeError()),
-    renderError: error => dispatch(renderError(error)),
-});
-
-export default connect(mapState, mapDispatch)(SignInContainer);
+export default SignInContainer;
