4841c5115a55f7a5bdeb449bdf031b458c203008
collin-tx <cmsmith.tx@gmail.com>
2021-05-03 22:56:50 -0500

    wip - books container

--- src/bookshop/Books/container.js
+++ src/bookshop/Books/container.js
@@ -4,63 +4,39 @@ import { useDispatch, useSelector } from 'react-redux';
 import Book from '../Book';
 import Books from './Books';
 
-import { quote } from '../../utils/helper';
-import { 
-    addBookToCart,
-    fetchBooks 
-} from '../../entities/books';
-import { 
-    getBooks,
-    getFirebase,
-    getQueries,
-    getUser,
-    getNoBooksFound
-} from '../../library/selectors';
+const BooksContainer = ({
+    addToCart,
+    books,
+    searchParams
+}) => {
+    const bookHasPrice = b => !!(b.saleInfo?.listPrice?.amount);
 
-const BooksContainer = () => {
+    let searchResults = books;
 
-    const [ term, setTerm ] = React.useState('');
-    const [ adding, setAdding ] = React.useState(false);
-    const [ loading, setLoading ] = React.useState(false);
-    const [ searched, setSearched ] = React.useState(false);
-    
-    const queries = useSelector(getQueries) ?? [];
-    const books = useSelector(getBooks);
-    const user = useSelector(getUser);
-    const noBooks = useSelector(getNoBooksFound);
-    const firebase = useSelector(getFirebase);
-
-    const dispatch = useDispatch();
-
-    const handleChange = e => setTerm(e.target.value);
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        searchForBooks(term);
-        setTerm('');
+    if (searchParams.canBuy) {
+        // filter
+        searchResults = books.filter(b => bookHasPrice(b));
     }
 
-    const addToCart = (e, index) => {
-        setAdding(true);
-        addBookToCart(firebase, books[index], user)(dispatch);
-        setAdding(false);
-    }
+    // TODO
+    // these need more work
+    // if (searchParams.genre) {
+    //     searchResults = books.filter(b => bookMatchesGenre(b));
+    // }
 
-    const searchForBooks = query => {
-        setLoading(true);
-        setSearched(true);
-        fetchBooks(query, firebase, queries)(dispatch);
-        setLoading(false);
-    }
 
-    const allBooks = books?.map((book, index) => {
+    // if (searchParams.author) {
+    //     searchResults = books.filter(b => bookHasPrice(b));
+    // }
+
+    const bookList = searchResults?.map((book, index) => {
         return (
             <Book 
                 title={book.volumeInfo.title} book={book}
                 author={book.volumeInfo.authors && book.volumeInfo.authors[0]} 
                 category={book.volumeInfo.categories && book.volumeInfo.categories[0]} 
                 description={book.volumeInfo.description}
-                price={book.saleInfo.listPrice && book.saleInfo.listPrice.amount} 
+                price={book.saleInfo?.listPrice?.amount || book.saleInfo?.retailPrice?.amount}
                 img={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail}
                 infoLink={book.volumeInfo.infoLink} 
                 preview={book.volumeInfo.previewLink} 
@@ -75,16 +51,7 @@ const BooksContainer = () => {
 
     return (
         <Books
-            adding={adding}
-            books={books}
-            bookList={allBooks}
-            loading={loading}
-            noBooks={noBooks}
-            onChange={handleChange}
-            onSubmit={handleSubmit} 
-            quote={quote}
-            searched={searched}
-            term={term}
+            bookList={bookList}
         />
     );
 }
