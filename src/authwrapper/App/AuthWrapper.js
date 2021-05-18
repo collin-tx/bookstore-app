0ee219f5dc6a19117d6dd0b2b89758da8402a492
collin-tx <cmsmith.tx@gmail.com>
2021-05-18 12:23:27 -0500

    moves Auth component to a better place

--- src/authwrapper/App/AuthWrapper.js
+++ src/authwrapper/App/AuthWrapper.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Footer from '../../bookshop/components/Footer';
+import Header from '../../bookshop/components/Header';
+import Nav from '../../bookshop/components/Nav';
+
 import SignIn from '../sign-in';
 
 const AuthWrapper = props => {
-
   const { firebase } = props;
-
   const  [ isNewUser, setIsNewUser ] = React.useState(true);
-
   const onClick = () => setIsNewUser(!isNewUser);
 
   return (
@@ -22,7 +25,16 @@ const AuthWrapper = props => {
           </div>
         </div>
     </div>
-  )
+  );
 }
 
-export default AuthWrapper;
+const Auth = ({firebase}) => (
+  <BrowserRouter>
+    <Nav firebase={firebase}/>
+    <Header />
+    <AuthWrapper firebase={firebase} />
+    <Footer />
+  </BrowserRouter>
+);
+
+export default Auth;
